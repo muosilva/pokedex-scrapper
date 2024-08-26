af8b5ecd19cd8f3e28b7dbde9d66697946ea0f29
Henrique Medeiros <84346920+henriquemcmedeiros@users.noreply.github.com>
2024-08-26 17:00:49 -0300

    Trocando a lógica das evoluções

--- pokedex/pokedex/spiders/pokedex_spider.py
+++ pokedex/pokedex/spiders/pokedex_spider.py
@@ -42,7 +42,7 @@ class PokedexSpider(scrapy.Spider):
             
             yield request
 
-            break
+            #break
 
     def parse_pokemon(self, response):
         item = response.meta['item']
@@ -51,55 +51,37 @@ class PokedexSpider(scrapy.Spider):
         altura = response.css('.vitals-table > tbody > tr:nth-child(4) > td::text').get()
         peso = response.css('.vitals-table > tbody > tr:nth-child(5) > td::text').get()
 
-        #evolucoes_element = response.css('div.infocard-list-evo > div.infocard > span.infocard-lg-data')
+        evolucoes_element = response.css('div.infocard-list-evo > div.infocard > span.infocard-lg-data')
+
+
         evolucao_elements = response.css('div.infocard-list-evo > div.infocard')
+        evolucao_split = response.css('span.infocard-evo-split')
 
-        #evolucoes_id = response.css('div.infocard-list-evo > div.infocard > span.infocard-lg-data > small:nth-child(1)::text').getall()
-        #evolucoes_nome = response.css('div.infocard-list-evo > div.infocard > span.infocard-lg-data > a::text').getall()
-        #evolucoes_link = response.css('div.infocard-list-evo > div.infocard > span.infocard-lg-data > a::attr(href)').getall()
-
-        evolucoes = []
-        passou_atual = False
-
-        for evolucao in evolucao_elements:
-            # Verifica se o elemento está dentro de uma div com a classe infocard-evo-split
-            inside_split = evolucao.xpath('ancestor::div[contains(@class, "infocard-evo-split")]').get()
-            
-            # Captura os dados da evolução
-            evolucao_id = evolucao.css('span.infocard-lg-data > small:nth-child(1)::text').get()
-            evolucao_nome = evolucao.css('span.infocard-lg-data > a::text').get()
-            evolucao_link = evolucao.css('span.infocard-lg-data > a::attr(href)').get()
-
-            # Armazena os dados com a flag inside_split
-            if (not passou_atual):
-                if (evolucao_id[1:] == item["numero"]):
-                    passou_atual = True
-            elif (inside_split == None and evolucao_id[1:] != item["numero"] and len(evolucao.css("small")) == 2 and evolucao not in evolucoes):
-                evolucoes.append({
-                    'numero': evolucao_id,
-                    'nome': evolucao_nome,
-                    'link': evolucao_link,
-                })
+        evolucoes_id = response.css('div.infocard-list-evo > div.infocard > span.infocard-lg-data > small:nth-child(1)::text').getall()
+        evolucoes_nome = response.css('div.infocard-list-evo > div.infocard > span.infocard-lg-data > a::text').getall()
+        evolucoes_link = response.css('div.infocard-list-evo > div.infocard > span.infocard-lg-data > a::attr(href)').getall()
 
         habilidade_nome = response.css('div.grid-row > div.grid-col:nth-child(2) > table > tbody > tr:nth-child(6) > td > .text-muted > a::text').getall()
         habilidade_link = response.css('div.grid-row > div.grid-col:nth-child(2) > table > tbody > tr:nth-child(6) > td > .text-muted > a::attr(href)').getall()
         habilidade_descricao = response.css('div.grid-row > div.grid-col:nth-child(2) > table > tbody > tr:nth-child(6) > td > .text-muted > a::attr(title)').getall()
 
-        habilidades= []
+        habilidades = []
+        evolucoes = []
+        passou_atual = False
 
-        #for i in range(len(evolucoes_id)):
-        #    if (not passou_atual):
-        #        if (evolucoes_id[i][1:] == item["numero"]):
-        #            passou_atual = True
-        #    #elif (evolucoes_id[i][1:] != item["numero"] and int(evolucoes_id[i][1:]) > int(item["numero"])):
-        #    elif (len(evolucoes_element[i].css("small")) == 2):
-        #        evolucao = {
-        #            "numero": evolucoes_id[i],
-        #            "nome": evolucoes_nome[i],
-        #            "link": evolucoes_link[i]
-        #        }
-        #        if evolucao not in evolucoes:
-        #            evolucoes.append(evolucao)
+        for i in range(len(evolucoes_id)):
+            if (not passou_atual):
+                if (evolucoes_id[i][1:] == item["numero"]):
+                    passou_atual = True
+            elif (len(evolucoes_element[i].css("small")) == 2):
+                if bool(evolucao_split.css(evolucao_elements)):
+                    evolucao = {
+                        "numero": evolucoes_id[i],
+                        "nome": evolucoes_nome[i],
+                        "link": evolucoes_link[i]
+                    }
+                    if evolucao not in evolucoes:
+                        evolucoes.append(evolucao)
 
         for i in range(len(habilidade_nome)):
             habilidade = {
@@ -120,4 +102,4 @@ class PokedexSpider(scrapy.Spider):
 # Tratamentos a serem feitos
 # Só deixar os números do Id, peso e altura
 # Se id 841 ou 842 não tem evolução
-# Incluir { "numero": 0413, "nome": Wormadam, "link": /pokedex/wormadam
+# Incluir { "numero": 0413, "nome": Wormadam, "link": /pokedex/wormadam } ao #0411
